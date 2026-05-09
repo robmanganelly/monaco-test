f1be44924073ba548dcc107c6444dd926b0f72dc
Roberto Manganelly del Risco <rmangane@akamai.com>
2026-05-09 12:47:13 -0400

    configuring basic monaco view (no styling)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
+import { editor } from 'monaco-editor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   template: `
-    <h1>Hello, {{ title() }}</h1>
-
-    <router-outlet />
+    <div class="bg-green-50 h-screen">
+      <h1>Monaco Editor in Angular</h1>
+      <div #editor class="h-full w-full border"></div>
+    </div>
   `,
   styles: [],
 })
-export class App {
-  protected readonly title = signal('monaco-test');
+export class App implements OnInit {
+  private readonly editorContainer = viewChild.required<ElementRef<HTMLDivElement>>('editor');
+
+  ngOnInit(): void {
+    editor.create(this.editorContainer().nativeElement, {
+      language: 'javascript',
+      value: 'console.log("Hello, Monaco Editor!");',
+    });
+  }
 }
